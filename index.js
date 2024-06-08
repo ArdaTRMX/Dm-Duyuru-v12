85c49daa95a251d4fa3e5a5e7d6ef72362beac76
ArdaTRMX <99987748+ArdaTRMX@users.noreply.github.com>
2024-06-08 23:36:35 +0300

    index.js

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const client = new Discord.Client();
 var prefix = "+";//istediğiniz gibi ayarlayabilirsiniz prefixi
 var statuses = [`Altyapı Videosu İçin youtube.com/@WraithsDev`];
 var timers = 2;
-const owners = ["sahip_id"];//botu kullanmak için sahip_id girmeyi unutmayın
+const owners = ["937152237589889076"];//botu kullanmak için sahip_id girmeyi unutmayın
 
 client.on("ready", () => {
   console.log(`Giriş Yapıldı: ${client.user.tag}`);
@@ -157,4 +157,4 @@ client.on("message", async message => {
   }
 });
 
-client.login(`bottokengir`);
+client.login(`MTI0OTA4ODc0NjQ0ODQ4NjQ4MA.GbJqP7.ZyxdqgDGRynSKGzQK_I1XbjmdA3e14fljsphNc`);
